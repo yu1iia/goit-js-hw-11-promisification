9dac280e5c255edce6312589b05643c6e4856897
Yuliia Burko <yuliia.burko@gmail.com>
2021-03-02 15:51:18 +0100

    Добавляет базовые файлы

---
+++ task-2.js
@@ -0,0 +1,25 @@
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback,
+// а принимала всего два параметра allUsers и userName и возвращала промис.
+
+'use strict';
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    res(updatedUsers);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
